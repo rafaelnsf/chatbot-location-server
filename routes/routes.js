18848ddd57f72a356fd7017c4c92d83a03ff922d
rafaelnsf <rafaelnsf@hotmail.com>
2023-05-06 18:41:04 -0300

    new function for take a spredsheet results

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const chatbot = require('../chatbot/chatbot')
+const { WebhookClient } = require("dialogflow-fulfillment");
 
 module.exports = app => {
     app.post('/text_query', async (req, res) => {
@@ -13,6 +14,65 @@ module.exports = app => {
         }
         res.send(resObject);
     })
+
+    app.post("/teste", function (request, response) {
+        const agent = new WebhookClient({ request: request, response: response });
+
+        //MAPEAMENTO DAS INTENTS
+
+        let intentMap = new Map();
+        intentMap.set('SALAS', pesquisa);
+        intentMap.set('teste', cadastro);
+        agent.handleRequest(intentMap);
+
+
+        //FUNÇÃO DE CADASTRO DE LEADS
+
+        function cadastro(agent) {
+            const { nomeSala, resultado } = agent.parameters;
+            const data = [{ NomeSala: nomeSala, Resultado: resultado }];
+
+            fetch("https://sheet.best/api/sheets/ec4edacc-4491-483c-af48-29e164f82199", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(() => {
+                    agent.add(`Bacana, Muito obrigado por suas informações pessoais.`);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+
+        // PESQUISAR CLIENTE E EVENTOS
+
+        function pesquisa(agent) {
+            var Sala = request.body.queryResult.parameters["nomeSala"];
+
+            fetch("https://sheet.best/api/sheets/ec4edacc-4491-483c-af48-29e164f82199")
+                .then(res => res.json())
+                .then(data => {
+                    data.forEach(coluna => {
+                        if (coluna.Sala === Sala) {
+                            response.json({
+                                fulfillmentText:
+                                    "Para chegar até a " +
+                                    "Sala: " +
+                                    coluna.NomeSala +
+                                    " Siga esses passos: " +
+                                    coluna.Resultado
+                            });
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    });
     // app.post('/event_query', (req, res) => {
     //     console.log(req);
     //     res.send("Text Query");
