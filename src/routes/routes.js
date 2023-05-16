8e05ce42064dad676492d4bf9c733105dc52977f
rafaelnsf <rafaelnsf@hotmail.com>
2023-05-15 22:19:50 -0300

    test fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -53,10 +53,10 @@ module.exports = app => {
         // PESQUISAR CLIENTE E EVENTOS
 
         async function pesquisa(request, response) {
-            console.log("######################################request", request);
+            // console.log("######################################request", request);
             // console.log("##################################response", response);
             try {
-                let Sala = request.parameters["salas"];
+                let Sala = request.body.queryResult.parameters["nomeSala"];
                 const imagens = [];
 
                 const res = await fetch(spreadsheetUrl);
